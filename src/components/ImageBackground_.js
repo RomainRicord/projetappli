16eaa1471761ad3a55933b036ceb0c2137da858a
Romain Ricord <romain.ricord.pro@gmail.com>
2022-05-23 09:45:09 +0200

    Rajout des images dans les assets

--- src/components/ImageBackground_.js
+++ src/components/ImageBackground_.js
@@ -1,8 +1,9 @@
 import {ImageBackground} from 'react-native'
 
 const ImageBackground_ = () => {
+    <ImageBackground source={require("../../assets/photo-1513104890138-7c749659a591.jpg")} resizeMode='cover'>
 
-
+    </ImageBackground>
 }
 
 export default ImageBackground_
--- src/screens/Welcome.js
+++ src/screens/Welcome.js
@@ -1,10 +1,11 @@
-import {Image} from 'react-native'
+import {Image,View} from 'react-native'
 import { Card,Button } from 'react-native-paper';
 import ImageBackground_ from '../components/ImageBackground_';
 
 const Welcome = () => {
     <ImageBackground_>
         
+        <Image source={require('../../assets/logo-289x300.png')} />
     </ImageBackground_>
 }
 
