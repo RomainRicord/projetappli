4028ab645deede686403af46048926c745d99c66
Romain Ricord <romain.ricord.pro@gmail.com>
2022-05-23 09:38:27 +0200

    Initialisation du projet sur GIT

---
+++ src/components/ImageBackground_.js
@@ -0,0 +1,8 @@
+import {ImageBackground} from 'react-native'
+
+const ImageBackground_ = () => {
+
+
+}
+
+export default ImageBackground_
---
+++ src/screens/Welcome.js
@@ -0,0 +1,11 @@
+import {Image} from 'react-native'
+import { Card,Button } from 'react-native-paper';
+import ImageBackground_ from '../components/ImageBackground_';
+
+const Welcome = () => {
+    <ImageBackground_>
+        
+    </ImageBackground_>
+}
+
+export default Welcome
