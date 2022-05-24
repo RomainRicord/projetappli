8df7a74abc5a315ba02a000e954bf0576ebc068e
Romain Ricord <romain.ricord.pro@gmail.com>
2022-05-24 09:25:18 +0200

    merge

--- src/data/montagnarde.js
+++ src/data/montagnarde.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> 8141becb6aba4fe0a49880a4d1b0ed07e6f05490
 const montagnarde = [
     {name:"Savoyarde",ingredients:"Crème, Mozzarella, Reblochon, Lardons, Pommes de terres, Origan",petite:"6,70€",moyenne:"10,00€",grande:"13,00€",picture: require("../../assets/img/savoyarde.jpeg")},
     {name:"Raclette",ingredients:"Crème, Mozzarella, Raclette, Jambont cuit et cru, Bacon, Pommes de terre",petite:"6,70€",moyenne:"10,00€",grande:"13,00€",picture: require("../../assets/img/raclette.jpg")}
 ]
 
-<<<<<<< HEAD
 export default montagnarde 
-=======
-export default montagnarde
->>>>>>> 8141becb6aba4fe0a49880a4d1b0ed07e6f05490
