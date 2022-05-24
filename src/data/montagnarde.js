53881c5fd5b77bebae4196a4509a7b72ddb857a4
Romain Ricord <romain.ricord.pro@gmail.com>
2022-05-24 09:23:24 +0200

    Tentative de résolution du bug d'affichage des pizzas

---
+++ src/data/montagnarde.js
@@ -0,0 +1,6 @@
+const montagnarde = [
+    {name:"Savoyarde",ingredients:"Crème, Mozzarella, Reblochon, Lardons, Pommes de terres, Origan",petite:"6,70€",moyenne:"10,00€",grande:"13,00€",picture: require("../../assets/img/savoyarde.jpeg")},
+    {name:"Raclette",ingredients:"Crème, Mozzarella, Raclette, Jambont cuit et cru, Bacon, Pommes de terre",petite:"6,70€",moyenne:"10,00€",grande:"13,00€",picture: require("../../assets/img/raclette.jpg")}
+]
+
+export default montagnarde 
---
+++ src/data/terroirs.js
@@ -0,0 +1,9 @@
+const terroirs = [
+    {name:"La rouxe",ingredients:"Base crème, Mozzarella, Jambon blanc, Camenbert, Pont l'évêque, crème",petite:"6,70€",moyenne:"10,00€",grande:"13,00€",picture: require("../../assets/img/roux.jpg")},
+    {name:"Chef",ingredients:"Tomates, Mozzarella, Jambon, Oignons, Poivrons, Chèvre, Persillade",petite:"6,50€",moyenne:"9,80€",grande:"12,80€",picture: require("../../assets/img/chef.jpg")},
+    {name:"Guariguette",ingredients:"Tomates, Mozzarella, Grogonzola, Jambon cru, Roquette",petite:"6,70€",moyenne:"10,00€",grande:"13,00€",picture: require("../../assets/img/guariguette.jpg")},
+    {name:"Ibiza",ingredients:"Tomates, Mozzarella, Chorizo, Oignons",petite:"6,70€",moyenne:"10,00€",grande:"13,00€",picture: require("../../assets/img/ibiza.jpg")},
+    {name:"Samba",ingredients:"Tomates pimentées, Mozzarella, Chorizo, Merguez, Crème",petite:"6,70€",moyenne:"10,00€",grande:"13,00€",picture: require("../../assets/img/samba.png")}
+]
+
+export default terroirs 
